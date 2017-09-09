c5f68031db2c784f03cfca555da5793b81354934
Jane Lee <Yesdoing13@gmail.com>
2017-09-09 14:40:45 +0900

    유저 프로필 보기 기능 추가

--- src/main/java/yesdoing/web/UserController.java
+++ src/main/java/yesdoing/web/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -37,5 +38,11 @@ public class UserController {
 	public String joinSave(User user) {
 		userRepository.save(user);
 		return "redirect:/users";
+	}
+	
+	@GetMapping("/{userId}")
+	public String profile(@PathVariable String userId, Model model) {
+		model.addAttribute("user", userRepository.findByUserId(userId));
+		return "user/profile";
 	}
 }
