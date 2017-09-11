2baee7696d11c802740aea06ce83787cf109adc9
Jane Lee <Yesdoing13@gmail.com>
2017-09-11 16:35:49 +0900

    delete 기능 추가

--- src/main/java/yesdoing/web/QuestionController.java
+++ src/main/java/yesdoing/web/QuestionController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,5 +73,21 @@ public class QuestionController {
 		return "redirect:/";
 	}
 	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable Long id, HttpSession session) {
+		User user = (User)session.getAttribute("sessionedUser");
+		if(user == null) {
+			return "user/login";
+		}
+		
+		Question question = questionRepository.findOne(id);
+		
+		if(!user.isSameWriter(question.getWriter())) {
+			return "redirect:/"; 
+		}
+				
+		questionRepository.delete(id);
+		return "redirect:/";
+	}
 	
 }
