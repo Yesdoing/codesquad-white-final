c5ed7f86d419e12f8948346a9780bbba6cfc9601
Jane Lee <Yesdoing13@gmail.com>
2017-09-11 18:54:59 +0900

    테스트 케이스 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO USER	 (id, user_id, password, name, email) VALUES (1, 'javajigi', 't
 INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
 INSERT INTO USER (id, user_id, password, name, email) VALUES (3, 'yesdoing', '1234', '그래하자고', 'yesdoing@slipp.net');
 
-INSERT INTO QUESTION (id, title, writer, contents) VALUES(1, 'hello', 'yesdoing', 'Hello World!');
-INSERT INTO QUESTION (id, title, writer, contents) VALUES(2, 'world!', 'yesdoing', 'Hello World!');
-INSERT INTO QUESTION (id, title, writer, contents) VALUES(3, 'Myname is yesdoing!', 'yesdoing', 'Hello World!');
+INSERT INTO QUESTION (id, title, writer_id, contents) VALUES(1, 'hello', 1, 'Hello World!');
+INSERT INTO QUESTION (id, title, writer_id, contents) VALUES(2, 'world!', 1, 'Hello World!');
+INSERT INTO QUESTION (id, title, writer_id, contents) VALUES(3, 'Myname is yesdoing!', 2, 'Hello World!');
